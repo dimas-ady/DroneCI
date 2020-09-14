da87d9981bcbc2a5f0444f5e4a2847457db1517a
ZyCromerZ <neetroid97@gmail.com>
2020-09-14 13:11:06 +0700

    fix
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 git clone https://$GITHUB_TOKEN@github.com/VISakura/KERNEL-X00T
 cd KERNEL-X00T
-curl https://raw.githubusercontent.com/VISakura/myscripts/kernel.sh | bash
+curl https://raw.githubusercontent.com/VISakura/myscripts/master/kernel.sh | bash

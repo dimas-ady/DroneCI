06a093fd9ef0a615f2cf507c4359678a1c74c399
VISakura <hazukashio@gmail.com>
2020-11-01 10:28:01 +0700

    V2
    Signed-off-by: VISakura <hazukashio@gmail.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://$GITHUB_TOKEN@github.com/VISakura/KERNEL-X00T
+git clone https://$GITHUB_TOKEN@github.com/VISakura/KERNEL-X00T -b V2
 cd KERNEL-X00T
-curl https://raw.githubusercontent.com/VISakura/myscripts/master/MASTER.sh | bash
+curl https://raw.githubusercontent.com/VISakura/myscripts/master/V2.sh | bash

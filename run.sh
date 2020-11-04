e15b5babc682e4ca04ddb3f683d6c131a7311042
Hazukashio <hazukashio@gmail.com>
2020-11-04 14:07:27 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/android-linux-stable/msm-4.4
+git clone https://github.com/android-linux-stable/msm-4.4 -b kernel.lnx.4.4.r38-rel
 cd msm-4.4
-git push https://$GIH_TOKEN@github.com/Takanashi-Hikari/HIKARI-X00T.git
+git push https://$GITHUB_TOKEN@github.com/Takanashi-Hikari/HIKARI-X00T.git

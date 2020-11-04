bcbd348f2fbcc227371869966941d8a1c6f8d19c
VISakura <hazukashio@gmail.com>
2020-11-04 05:46:23 +0000

    Hmmm
    Signed-off-by: VISakura <hazukashio@gmail.com>

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-git clone -b kernel.lnx.4.4.r38-rel --single-branch https://github.com/android-linux-stable/msm-4.4 Kernel && cd Kernel && git push https://$GH_TOKEN@github.com/Takanashi-Hikari/HIKARI-X00T
+git clone -b kernel.lnx.4.4.r38-rel --single-branch https://github.com/android-linux-stable/msm-4.4 Kernel && cd Kernel && git push https://$GIH_TOKEN@github.com/Takanashi-Hikari/HIKARI-X00T

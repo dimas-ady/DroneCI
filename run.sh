12b17342541af23addf1055b677d2ceb04e78aa1
Hazukashio <hazukashio@gmail.com>
2020-11-13 00:28:51 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-git clone https://github.com/travarilo/kernel_asus_sdm660 -b ten-eas
-cd kernel_asus_sdm660
+git clone https://source.codeaurora.org/quic/la/kernel/msm-4.4 -b kernel.lnx.4.4.r42-rel
+cd msm-4.4
+git checkout -b AUR-42
 git push https://$GITHUB_TOKEN@github.com/Takanashi-Hikari/CAF-TEA.git

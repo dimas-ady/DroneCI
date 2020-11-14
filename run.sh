2813c7b7c6a607825df0b78ef6ae5972e1d1f657
Hazukashio <hazukashio@gmail.com>
2020-11-14 13:56:04 +0700

    205

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/psndna88/AGNi_pureMIUI -b LA.UM.8.2.1.r1-05700-sdm660-4.14
-cd AGNI_pureMIUI
-git checkout -b CAF-4.14
+git clone https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git -b v4.4.205
+cd linux
+git checkout -b LEV
 git push https://$GITHUB_TOKEN@github.com/Takanashi-Hikari/CAF-TEA.git

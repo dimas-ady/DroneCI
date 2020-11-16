400bace6131f4573b2e2746704334a9d0a263092
Hazukashio <hazukashio@gmail.com>
2020-11-16 18:27:13 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 git clone https://source.codeaurora.org/quic/la/kernel/msm-4.4 -b kernel.lnx.4.4.r42-rel
 cd msm-4.4
 git checkout -b LEV
-git push https://$GITHUB_TOKEN@github.com/Takanashi-Hikari/CAF-TEA.git
+git push https://$GITHUB_TOKEN@github.com/Takanashi-Hikari/TEA-PROJECT.git

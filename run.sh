7404d92e3e6c541c9c198e0eb5a502bc6b65d7a9
Hazukashio <hazukashio@gmail.com>
2020-11-17 00:29:42 +0700

    Gcc Arm

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-git clone https://github.com/android-linux-stable/msm-4.4 -b kernel.lnx.4.4.r38-rel
-cd msm-4.4
-git branch -M main
-git push https://$GITHUB_TOKEN@github.com/Takanashi-Hikari/TEA-PROJECT.git
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b ndk-release-r22
+cd arm-linux-androideabi-4.9
+git init
+git add -A
+git commit -m -f
+git branch -M ARM
+git push https://$GITHUB_TOKEN@github.com/Takanashi-Hikari/arm-linux-androideabi-4.9.git

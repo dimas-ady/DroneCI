fae3e542af8fe1ef218fa98c351c513b6031846f
Hazukashio <hazukashio@gmail.com>
2020-11-16 18:36:22 +0700

    Help My Friend ;8

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-git clone https://source.codeaurora.org/quic/la/kernel/msm-4.4 -b kernel.lnx.4.4.r42-rel
+git clone https://github.com/android-linux-stable/msm-4.4 -b kernel.lnx.4.4.r38-rel
 cd msm-4.4
 git branch -M main
 git push https://$GITHUB_TOKEN@github.com/Takanashi-Hikari/TEA-PROJECT.git

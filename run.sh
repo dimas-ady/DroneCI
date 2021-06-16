7be6a23cfa770b39f8132786ed14d8cf71679f94
dimas-ady <dimasady123@outlook.co.id>
2021-06-16 15:33:26 +0700

    Fix because I forgot to change Folder Name

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-#git clone https://github.com/dimas-ady/kernel_asus_sdm660.git
-#cd KERNEL-X00T
-#bash build.sh
-cd KERNEL-X00T | ls
+git clone https://github.com/dimas-ady/kernel_asus_sdm660.git
+cd kernel_asus_sdm660
+bash build.sh

49a1bceabc85d2825a64add77ab612160a8f424c
dimas-ady <dimasady123@outlook.co.id>
2021-06-16 16:27:04 +0700

    I forgot this

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #git clone https://github.com/dimas-ady/kernel_asus_sdm660.git
 git clone https://github.com/LineageOS/android_kernel_asus_sdm660.git
-cd kernel_asus_sdm660
+cd android_kernel_asus_sdm660
 bash ../build.sh

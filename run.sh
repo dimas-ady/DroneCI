e6ca129368d1b5f44d6ab17662c4bfe5199f0842
dimas-ady <dimasady123@outlook.co.id>
2021-06-16 15:52:01 +0700

    I hope fixed

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
-#git clone https://github.com/dimas-ady/kernel_asus_sdm660.git
-#cd kernel_asus_sdm660
-mkdir test
-cd test
-mkdir a
-ls
+git clone https://github.com/dimas-ady/kernel_asus_sdm660.git
+cd kernel_asus_sdm660
 bash ../build.sh

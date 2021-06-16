1b18eb2571d30ea797aa45c8ed01babccc514ebe
dimas-ady <dimasady123@outlook.co.id>
2021-06-16 15:46:11 +0700

    Salah mulu awoakwok

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-git clone https://github.com/dimas-ady/kernel_asus_sdm660.git
-cd kernel_asus_sdm660
-./bash build.sh
+#git clone https://github.com/dimas-ady/kernel_asus_sdm660.git
+#cd kernel_asus_sdm660
+mkdir test
+cd test
+mkdir a
+ls
+bash ./build.sh

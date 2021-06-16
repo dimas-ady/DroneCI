56512ee84313073dc5f25c8a0658f5aa7ea682aa
dimas-ady <dimasady123@outlook.co.id>
2021-06-16 15:31:59 +0700

    iwtt

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-git clone https://github.com/dimas-ady/kernel_asus_sdm660.git
-cd KERNEL-X00T
-bash build.sh
+#git clone https://github.com/dimas-ady/kernel_asus_sdm660.git
+#cd KERNEL-X00T
+#bash build.sh
+cd KERNEL-X00T | ls

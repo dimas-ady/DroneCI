93d605e6a1e7884b223daeaae028b083f8f2f85f
dimas-ady <dimasady123@outlook.co.id>
2021-06-16 19:10:15 +0700

    Lol

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #git clone https://github.com/dimas-ady/kernel_asus_sdm660.git
-git clone https://github.com/LineageOS/android_kernel_asus_sdm660.git -b "lineage-18.1"
+git clone -b "lineage-18.1" https://github.com/LineageOS/android_kernel_asus_sdm660.git
 cd android_kernel_asus_sdm660
 bash ../build.sh

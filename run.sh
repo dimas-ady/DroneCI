7d1183b18df486e8a3856dc430a2e614fbd4c6e9
dimas-ady <dimasady123@outlook.co.id>
2021-06-17 15:25:00 +0700

    Test GCC 4.9 to compile Los 16 Kernel

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 # Clone the kernel and run the build script
 
-git clone -b "lineage-18.1" https://github.com/LineageOS/android_kernel_asus_sdm660.git
+#git clone -b "lineage-18.1" https://github.com/LineageOS/android_kernel_asus_sdm660.git
+git clone https://github.com/LineageOS/android_kernel_asus_sdm660.git
 cd android_kernel_asus_sdm660
 bash ../build.sh

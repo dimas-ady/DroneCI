14ad2873da5bf99217e884796f3edef6cb6aa661
dimas-ady <dimasady123@outlook.co.id>
2021-06-17 08:36:21 +0700

    yy

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 TEST="yo 10"
 
-if [ "$TEST" == "yo 10"]
+if [ "$TEST" == "yo 10"];
 then
 git clone -b "lineage-18.1" https://github.com/LineageOS/android_kernel_asus_sdm660.git
 cd android_kernel_asus_sdm660

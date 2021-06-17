4108e4295d312ee555fd32006db329219526e857
dimas-ady <dimasady123@outlook.co.id>
2021-06-17 16:51:40 +0700

    Try to upstream 4.4.273

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 git clone -b "lineage-18.1" https://github.com/LineageOS/android_kernel_asus_sdm660.git
 #git clone https://github.com/LineageOS/android_kernel_asus_sdm660.git
 cd android_kernel_asus_sdm660
-bash ../build.sh
+git remote add upstream https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/ && git fetch upstream v4.4.273 && git merge FETCH_HEAD
+#bash ../build.sh

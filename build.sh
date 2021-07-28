b4c6fcc064960bb10c76d1a2dd9e2a1132120e05
dimas-ady <dimasady123@outlook.co.id>
2021-07-28 07:29:51 +0700

    update

--- build.sh
+++ build.sh
@@ -50,7 +50,7 @@ DEFCONFIG=X00TD_defconfig
 
 # Specify compiler. 
 # 'clang' or 'gcc'
-COMPILER="nusantara clang"
+COMPILER="clang"
   if [ "$COMPILER" == "gcc 4.9" || "$COMPILER" == "gcc 10" || $COMPILER == "gcc linaro" ]
   then
     IS_GCC=Y

b2478cef3420da4673e6f2dcc965b77f1d2d1a14
dimas-ady <dimasady123@outlook.co.id>
2021-07-28 07:40:28 +0700

    change

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # Clone the kernel and run the build script
 
-#git clone https://github.com/dimas-ady/kernel_asus_sdm660.git kernel
-git clone https://github.com/travarilo/kernel_asus_sdm660 kernel
+git clone https://github.com/dimas-ady/kernel_asus_sdm660.git kernel
+#git clone https://github.com/travarilo/kernel_asus_sdm660 kernel
 cd kernel
 bash ../build.sh

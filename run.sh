5d14b95ba0f7153adae678b9ce398abb947217f0
dimas-ady <dimasady123@outlook.co.id>
2021-07-28 07:39:36 +0700

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 # Clone the kernel and run the build script
 
 #git clone https://github.com/dimas-ady/kernel_asus_sdm660.git kernel
-#cd kernel
-git clone -b "lineage-18.1" https://github.com/LineageOS/android_kernel_asus_sdm660.git
-cd android_kernel_asus_sdm660
+git clone https://github.com/travarilo/kernel_asus_sdm660 kernel
+cd kernel
 bash ../build.sh
